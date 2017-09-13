2dd2fa87c111235ca51993e5f01dca76bb288bb4
betogontijo <maximoussk8@yahoo.com.br>
2017-09-12 21:52:21 -0300

    Initial commit.

---
+++ src/main/java/br/com/betogontijo/sbgindexer/App.java
@@ -0,0 +1,13 @@
+package br.com.betogontijo.sbgindexer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
